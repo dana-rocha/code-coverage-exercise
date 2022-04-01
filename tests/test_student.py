3969a6f78f4176b2d129e7ca6ee8891271c2d649
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-01 13:46:51 -0700

    passed all tests and coverage

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,17 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_empty_courses():
+    name = "Ada Lovelace"
+    level = "sophomore"
+
+    ada = Student(name, level)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == []
+
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -41,4 +52,10 @@ def test_get_student_with_more_classes():
         ["mathematics", "foundations of computing"]
     )
 
+    more_classes_student = get_student_with_more_classes(charles, ada)
+
+    more_classes_student2 = get_student_with_more_classes(ada, charles)
+
     # TODO: write assertions
+    assert more_classes_student == ada 
+    assert more_classes_student2 == ada
